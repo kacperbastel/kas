62250232a07842e395729a7fdfb26f917ce8d320
Kacper <kacperbastel@gmail.com>
2025-02-23 10:52:18 +0100

    Update ka.py

--- ka.py
+++ ka.py
@@ -2,7 +2,7 @@ def check_number(n):
     if n > 0:
         return "положительное"
     elif n < 0:
-        return "отрицательное"
+        return "min"
     else:
         return "ноль"
     else:
